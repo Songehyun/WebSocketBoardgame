a623c0fd6fa71e2be2feb95d4bc2ddf3c567b4ce
songehyun <songehyun00@gmail.com>
2024-09-01 17:31:27 +0900

    :bug: 'dest'가 undefined일 수 있는 경우를 처리하여 오류 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/macro/rollAndMove.ts', // 번들링할 입력 파일
-  output: {
-    filename: 'rollAndMove.bundle.js', // 출력 파일명
-    path: path.resolve(__dirname, 'dist'), // 출력 디렉토리
-  },
+  entry: './src/index.ts',
   module: {
     rules: [
       {
-        test: /\.ts$/, // .ts 파일에 대해
-        use: 'babel-loader', // Babel을 사용하여 변환
+        test: /\.ts$/,
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'], // .ts와 .js 확장자 파일을 인식
+    extensions: ['.ts', '.js'],
   },
-  devtool: 'source-map', // 디버깅을 위한 소스 맵 생성
+  output: {
+    filename: 'script.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './',
+  },
+  mode: 'development',
 };
